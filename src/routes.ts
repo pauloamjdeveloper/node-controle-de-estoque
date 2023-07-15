ae95b97baf62260ede5d60f062e757715b641dee
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-15 14:26:06 -0300

    Implementando busca do usuário por id

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 router.get("/test", (request: Request, response: Response) => {
@@ -9,5 +11,6 @@ router.get("/test", (request: Request, response: Response) => {
 
 router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
