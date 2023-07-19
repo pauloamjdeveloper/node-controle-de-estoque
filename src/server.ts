549c518cb864a31f79628fea1f98d55c42a0bac8
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-19 14:13:36 -0300

    Implementando a documentação do swagger

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const port = 3333;
 
 app.use(express.json());
 app.use(cors());
-app.use("v1", router);
+app.use("/v1", router);
 app.use( "/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
@@ -35,5 +35,5 @@ app.get("/terms", (request: Request, response: Response) => {
 });
 
 app.listen(port, () => {
-    console.log("Servidor rodando na porta 3333 - Projeto Controle de Estoque");
+    console.log("Server running on port 3333 - Stock Control Project");
 });
